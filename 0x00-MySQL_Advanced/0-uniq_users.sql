ded7a3116ea451e3ca3c362d351db09e0dccd740
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-03-22 05:42:33 +0300

    Added drop

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
 -- SQL that creates id, emai, name tables
-
+DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
