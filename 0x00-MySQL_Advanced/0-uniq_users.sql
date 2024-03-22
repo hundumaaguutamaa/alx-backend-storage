300b47d1907aa654d527b33271ea4faa2de0404a
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-03-22 05:47:47 +0300

    Updated table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,7 @@
 -- SQL that creates id, emai, name tables
 DROP TABLE IF EXISTS users;
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
-
