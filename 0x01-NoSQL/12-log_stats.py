8e2da4a3bc2b68d3e8f0eaf9e7b23e1774ab749b
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-03-27 05:15:08 +0300

    Implementing methods, document

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
-"""Provides some stats about Nginx logs stored in MongoDB."""
+"""Provides stats about Nginx logs stored in MongoDB."""
 from pymongo import MongoClient
+
+if __name__ == "__main__":
+  """ Connect to mongodb """
+  client = MongoClient('mongodb://127.0.0.1:27017')
+  db = client['logs']
+  collection = db['nginx']
+
+ print(f"{collection.estimated_document_count()} logs")
+
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        method_counts = collection.count_documents({'method': method})
+        print(f"method {method}: {method_counts}")
+
+    check_get = collection.count_documents(
+        {'method': 'GET', 'path': "/status"})
+    print(f"{check_get} status check")
